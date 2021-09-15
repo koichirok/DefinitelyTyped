ac3d39f9a3db2a03424014f0f71ad6296b25ea3f
KIKUCHI Koichiro <koichiro@rworks.jp>
2021-09-15 17:37:40 +0900

    feat: update dns2 to 2.0.1
    * update definitions for 2.0.1.
    * refine definitions by using module declaration.

--- types/dns2/dns2-tests.ts
+++ types/dns2/dns2-tests.ts
@@ -1,33 +1,139 @@
-// Examples taken from the package readme for 1.4.2:
-// https://github.com/song940/node-dns/tree/6c2041b82ba2109fbb95bc529572404508787337#dns2
+// Based on examples taken from the package readme for 2.0.1:
+// https://github.com/song940/node-dns/tree/fd953b3643c1423d7455ab352468549bab0638c1
 
-import DNS = require('dns2');
+import {RemoteInfo} from 'dgram';
+import {DnsQuestion, DnsRequest, DnsResponse, DNSServer} from 'dns2';
 
-const dns = new DNS();
+// DNS Client (default UDP)
+(() => {
+    const dns2 = require('dns2');
 
-(async () => {
-  const result = await dns.resolveA('google.com');
-  console.log(result.answers);
+    const options = {
+        // available options
+        // dns: dns server ip address or hostname (string),
+        // port: dns server port (number),
+        // recursive: Recursion Desired flag (boolean, default true, since > v1.4.2)
+    };
+    const dns = new dns2(options);
+
+    (async () => {
+        const result = await dns.resolveA('google.com');
+        console.log(result.answers);
+    })();
 })();
 
-const { Packet } = DNS;
+/// Another way to instanciate dns2 UDP Client:
+(() => {
+    const {UDPClient} = require('dns2');
 
-const server = DNS.createServer((request, send, rinfo) => {
-  const response = Packet.createResponseFromRequest(request);
-  const [ question ] = request.questions;
-  const { name } = question;
-  response.answers.push({
-    name,
-    type: Packet.TYPE.A,
-    class: Packet.CLASS.IN,
-    ttl: 300,
-    address: '8.8.8.8'
-  });
-  send(response);
-});
+    const resolve = UDPClient();
 
-server.on('request', (request, response, rinfo) => {
-  console.log(request.header.id, request.questions[0]);
-});
+    (async () => {
+        const response = await resolve('google.com')
+        console.log(response.answers);
+    })();
+})();
 
-server.listen(5333);
+// DNS Client (TCP)
+(() => {
+    const {TCPClient} = require('dns2');
+
+    const resolve = TCPClient();
+
+    (async () => {
+        try {
+            const response = await resolve('lsong.org')
+            console.log(response.answers);
+        } catch (error) {
+            // some DNS servers (i.e cloudflare 1.1.1.1, 1.0.0.1)
+            // may send an empty response when using TCP
+            console.log(error);
+        }
+    })();
+})();
+
+// Client Custom DNS Server
+(() => {
+
+    const {TCPClient} = require('dns2');
+
+    const resolve = TCPClient({
+        dns: '1.1.1.1'
+    });
+
+    (async () => {
+        try {
+            const result = await resolve('google.com');
+            console.log(result.answers);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})();
+
+// System DNS Server
+(() => {
+
+    const dns = require('dns');
+    const {TCPClient} = require('dns2');
+
+    const resolve = TCPClient({
+        dns: dns.getServers()[0]
+    });
+
+    (async () => {
+        try {
+            const result = await resolve('google.com');
+            console.log(result.answers);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})();
+
+// Example Server
+(() => {
+    const dns2 = require('dns2');
+
+    const {Packet} = dns2;
+
+    const server: DNSServer = dns2.createServer({
+        udp: true,
+        handle: (
+            request: DnsRequest,
+            send: (response: DnsResponse) => void,
+            rinfo: RemoteInfo
+        ) => {
+            const response: DnsResponse = Packet.createResponseFromRequest(request);
+            const [question]: DnsQuestion[] = request.questions;
+            const {name} = question;
+            response.answers.push({
+                name,
+                type: Packet.TYPE.A,
+                class: Packet.CLASS.IN,
+                ttl: 300,
+                address: '8.8.8.8'
+            });
+            send(response);
+        }
+    });
+
+    server.on('request', (request, response, rinfo) => {
+        console.log(request.header.id, request.questions[0]);
+    });
+
+    server.on('listening', () => {
+        console.log(server.addresses());
+    });
+
+    server.on('close', () => {
+        console.log('server closed');
+    });
+
+    server.listen({
+        udp: 5333
+    });
+
+    // eventually
+    server.close();
+})();
--- types/dns2/index.d.ts
+++ types/dns2/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dns2 1.4
+// Type definitions for dns2 2.0.1
 // Project: https://github.com/song940/node-dns#readme
 // Definitions by: Tim Perry <https://github.com/pimterry>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -7,87 +7,161 @@
 
 import * as net from 'net';
 import * as udp from 'dgram';
+import {EventEmitter} from 'events';
 
-declare class Packet {
-    static TYPE: {
-        A: 0x01;
-        NS: 0x02;
-        MD: 0x03;
-        MF: 0x04;
-        CNAME: 0x05;
-        SOA: 0x06;
-        MB: 0x07;
-        MG: 0x08;
-        MR: 0x09;
-        NULL: 0x0a;
-        WKS: 0x0b;
-        PTR: 0x0c;
-        HINFO: 0x0d;
-        MINFO: 0x0e;
-        MX: 0x0f;
-        TXT: 0x10;
-        AAAA: 0x1c;
-        SRV: 0x21;
-        EDNS: 0x29;
-        SPF: 0x63;
-        AXFR: 0xfc;
-        MAILB: 0xfd;
-        MAILA: 0xfe;
-        ANY: 0xff;
-        CAA: 0x101;
-    };
+declare module "dns2" {
 
-    static CLASS: {
-        IN: 0x01;
-        CS: 0x02;
-        CH: 0x03;
-        HS: 0x04;
-        ANY: 0xff;
-    };
+    namespace Packet {
+        enum TYPE {
+            A = 0x01,
+             NS = 0x02,
+             MD = 0x03,
+             MF = 0x04,
+            CNAME = 0x05,
+            SOA = 0x06,
+            MB = 0x07,
+            MG = 0x08,
+            MR = 0x09,
+            NULL = 0x0a,
+            WKS = 0x0b,
+            PTR = 0x0c,
+            HINFO = 0x0d,
+            MINFO = 0x0e,
+            MX = 0x0f,
+            TXT = 0x10,
+            AAAA = 0x1c,
+            SRV = 0x21,
+            EDNS = 0x29,
+            SPF = 0x63,
+            AXFR = 0xfc,
+            MAILB = 0xfd,
+            MAILA = 0xfe,
+            ANY = 0xff,
+            CAA = 0x101,
+        }
 
-    static createResponseFromRequest(request: DNS.DnsRequest): DNS.DnsResponse;
+        enum CLASS {
+            IN = 0x01,
+            CS = 0x02,
+            CH = 0x03,
+            HS = 0x04,
+            ANY = 0xff,
+        }
 
-    toBuffer(): Buffer;
-}
+        function createResponseFromRequest(request: DnsRequest): DnsResponse;
+    }
 
-declare namespace DNS {
+    interface Packet {
+        toBuffer(): Buffer;
+    }
+
     interface DnsRequest {
-        header: { id: string };
+        header: {id: string};
         questions: DnsQuestion[];
     }
 
     interface DnsQuestion {
         name: string;
+        type: Packet.TYPE;
+        class: Packet.CLASS;
     }
 
     interface DnsResponse {
-        answers: DnsAnswer[];
+        answers: Array<AOrAaaaRecord | CnameOrPtrRecord | MxRecord | NsRecord | SoaRecord | TxtRecord>;
+        authorities: Array<MxRecord | NsRecord>;
+        additionals: Array<SoaRecord | TxtRecord>;
     }
 
-    interface DnsAnswer {
+    /* @internal */ interface DnsRecord {
         name: string;
         type: number;
-        class: number;
+        class: Packet.CLASS.IN;
         ttl: number;
+    }
+
+    interface AOrAaaaRecord extends DnsRecord {
+        type: Packet.TYPE.A | Packet.TYPE.AAAA;
         address: string;
     }
+
+    interface CnameOrPtrRecord extends DnsRecord {
+        type: Packet.TYPE.CNAME | Packet.TYPE.PTR;
+        domain: string;
+    }
+
+    interface MxRecord extends DnsRecord {
+        type: Packet.TYPE.MX;
+        exchange: string;
+        priority: number;
+    }
+
+    interface NsRecord extends DnsRecord {
+        type: Packet.TYPE.NS;
+        ns: string;
+    }
+
+    interface SoaRecord extends DnsRecord {
+        type: Packet.TYPE.SOA;
+        primary: string;
+        admin: string;
+        serial: string;
+        refresh: number;
+        retry: number;
+        expiration: number;
+        minimum: number
+    }
+
+    interface TxtRecord extends DnsRecord {
+        type: Packet.TYPE.TXT;
+        data: string;
+    }
+
+    type DnsHandler = (
+        request: DnsRequest,
+        sendResponse: (response: DnsResponse) => void,
+        remoteInfo: udp.RemoteInfo,
+    ) => void;
+
+    interface AddressesInfo {
+        udp?: net.AddressInfo;
+        tcp?: net.AddressInfo;
+        doh?: net.AddressInfo;
+    }
+
+    class DNSServer extends EventEmitter {
+        addresses(): AddressesInfo;
+
+        listen(ports?: {
+            udp?: number,
+            tcp?: number,
+            doh?: number
+        }): Promise<AddressesInfo>;
+
+        close(): Promise<void>;
+    }
+
+    class UDPServer extends udp.Socket {
+        constructor(callback?: DnsHandler);
+        listen(port: number, address: string): Promise<void>;
+    }
+
+    class TCPServer extends net.Server {
+        constructor(callback?: DnsHandler);
+    }
+
+    function createServer(options: {
+        udp?: boolean,
+        tcp?: boolean,
+        doh?: boolean,
+        handle: DnsHandler
+    }): DNSServer;
+
+    function createUDPServer(...options: ConstructorParameters<typeof UDPServer>): UDPServer;
+
+    function createTCPServer(...options: ConstructorParameters<typeof TCPServer>): TCPServer;
+
+    function resolveA(domain: string, clientIp?: string): Promise<DnsResponse>;
+    function resolveAAAA(domain: string): Promise<DnsResponse>;
+    function resolveMX(domain: string): Promise<DnsResponse>;
+    function resolveCNAME(domain: string): Promise<DnsResponse>;
 }
-
-declare class DNS {
-    static createServer(
-        callback: (
-            request: DNS.DnsRequest,
-            sendResponse: (response: DNS.DnsResponse) => void,
-            remoteInfo: udp.RemoteInfo,
-        ) => void,
-    ): net.Server;
-
-    static Packet: typeof Packet;
-
-    resolveA(domain: string, clientIp?: string): Promise<DNS.DnsResponse>;
-    resolveAAAA(domain: string): Promise<DNS.DnsResponse>;
-    resolveMX(domain: string): Promise<DNS.DnsResponse>;
-    resolveCNAME(domain: string): Promise<DNS.DnsResponse>;
-}
-
-export = DNS;
